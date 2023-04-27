9e39f94821f246f28feb4ece413b4ca896ead63c
MGreek <marcgrec2003@gmail.com>
2023-04-27 17:36:54 +0300

    registration

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Database, ref, set, onValue, get } from '@angular/fire/database'
+import { User } from 'src/models/user';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  @ViewChild('username') username?:ElementRef;
+  @ViewChild('elo') elo?:ElementRef;
 
+  rows: User[] = []
+  constructor(public db: Database) {
+    onValue(ref(this.db, 'users'), (snapshot) => {
+      this.updateTable(snapshot);
+    });
+  }
+
+  registerUser()
+  {
+    if (!this.rows.some((val) => { return val.username === this.username?.nativeElement.value; }))
+    {
+      set(ref(this.db, 'users/' + this.username?.nativeElement.value), {
+        username: this.username?.nativeElement.value,
+        elo: this.elo?.nativeElement.value
+      });
+    }
+    else
+    {
+      alert("Username taken.");
+    }
+  }
+
+  updateTable(snapshot: any) {
+    this.rows = [];
+    snapshot.forEach((childSnapshot: any) => {
+      this.rows.push({ username: childSnapshot.val().username, elo: childSnapshot.val().elo });
+    })
+  }
 }
